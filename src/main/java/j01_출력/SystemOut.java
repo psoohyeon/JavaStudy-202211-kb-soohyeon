63163b4f8310cb73ef94d59e97dfe42de6c05590
psoohyeon <hyeony0007@naver.com>
2022-11-15 12:21:50 +0900

    챕터1 출력

---
+++ src/main/java/j01_출력/SystemOut.java
@@ -0,0 +1,16 @@
+package j01_출력;
+
+public class SystemOut {
+    public static void main(String[] args) {
+        System.out.println("이름:");
+        System.out.println("박수현");
+        System.out.println("주소: ");
+        System.out.println("부산 중구 남포동");
+        System.out.println("연락처: ");
+        System.out.println("010-7773-6413");
+        System.out.println();
+
+
+    }
+
+}
---
+++ src/main/java/j01_출력/UserInfo.java
@@ -0,0 +1,15 @@
+package j01_출력;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "박**";
+        String address = "부산 중구 남포동";
+        String phone = "010-0123-4567";
+        int age = 30;
+
+        System.out.println(name + "의 주소는 " + address + "입니다.");
+        System.out.println(name + "의 연락처는 " + phone + "입니다.");
+        System.out.println(name + "의 나이는 " + age + "살 입니다.");
+
+    }
+}
